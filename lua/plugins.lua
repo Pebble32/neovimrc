d988732891b00bfc49052e5fb1f4aa7d889d92e1
Pebble32 <bjkk32@gmail.com>
2025-01-19 13:26:53 +0000

    added diffview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,5 +2,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {'wbthomason/packer.nvim', opt = true}
+
+  -- deffview
+  use { "sindrets/diffview.nvim" }
 end)
