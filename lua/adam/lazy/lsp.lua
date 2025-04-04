ccf9fe63d7e71f042230460efb0ded0b1a651162
Pebble32 <bjkk32@gmail.com>
2025-04-04 08:44:52 +0000

    Added custome path to pyright

--- lua/adam/lazy/lsp.lua
+++ lua/adam/lazy/lsp.lua
@@ -14,36 +14,75 @@ return {
     },
 
     config = function()
-        local cmp = require('cmp')
+        -- Set up completion capabilities
+        local cmp = require("cmp")
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
             vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+            cmp_lsp.default_capabilities()
+        )
 
+        -- Setup fidget to show LSP progress
         require("fidget").setup({})
+
+        -- Set up Mason
         require("mason").setup()
+
+        -- Define the on_attach function to map keybindings after the LSP attaches to a buffer
+        local function on_attach(client, bufnr)
+            local opts = { buffer = bufnr, silent = true, noremap = true }
+
+            -- LSP Navigation
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)      -- Go to definition
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)       -- Go to declaration
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)         -- Go to references
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)     -- Go to implementation
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)               -- Show documentation
+
+            -- LSP Actions
+            vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts)  -- Signature help
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)            -- Rename symbol
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)       -- Code actions
+        end
+
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls", -- Lua
+                "lua_ls",        -- Lua
                 "rust_analyzer", -- Rust
-                "pyright", -- Python
-                "gopls", -- Go
-                "clangd" -- C/C++
-                -- "jdtls", -- Java
+                "pyright",       -- Python
+                "gopls",         -- Go
+                "clangd",        -- C/C++
+                -- "jdtls",      -- Java 
             },
             handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
+                -- Default handler for all servers
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
                         capabilities = capabilities,
+                        on_attach = on_attach,
+                    })
+                end,
+                -- Custom handler for Pyright to add extra paths
+                ["pyright"] = function()
+                    require("lspconfig").pyright.setup({
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = {
+                            python = {
+                                analysis = {
+                                    extraPaths = {"../libdohop"}
+                                }
+                            }
+                        }
+                    })
+                end,
+                -- Custom handler for Lua LS with extra diagnostic globals
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup({
+                        capabilities = capabilities,
+                        on_attach = on_attach,
                         settings = {
                             Lua = {
                                 diagnostics = {
@@ -51,60 +90,35 @@ return {
                                 }
                             }
                         }
-                    }
+                    })
                 end,
             }
         })
-
-        local function on_attach(client, bufnr)
-            local opts = { buffer = bufnr, silent = true, noremap = true }
-
-            -- LSP Navigation
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- Go to definition
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- Go to declaration
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts) -- Go to references
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- Go to implementation
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Show documentation
-
-            -- LSP Actions
-            vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts) -- Show signature
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Rename symbol eg. func
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Show actions
-        end
-
-
-        local lspconfig = require("lspconfig")
-        for _, server in ipairs({ "pyright", "gopls", "lua_ls" }) do
-            lspconfig[server].setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-        end
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
+        -- Configure diagnostics
         vim.diagnostic.config({
-            -- update_in_insert = true,
             float = {
                 focusable = false,
                 style = "minimal",
